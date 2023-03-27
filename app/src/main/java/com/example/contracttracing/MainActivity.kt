6571d814235e9a4e4eef9fd5dbe84ba38f0cc048
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-03-27 19:28:46 -0400

    Add binding and create layout for adding contact

--- app/src/main/java/com/example/contracttracing/MainActivity.kt
+++ app/src/main/java/com/example/contracttracing/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.contracttracing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.contracttracing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //
+        setContentView(binding.root)
+
+        binding.addContact.setOnClickListener(){
+            val intent = Intent(this, AddContact::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "This is test message", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }

de453d015644a3d0d1f86d0768559b5c3ae85595
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-03-27 21:07:20 -0400

    Create blueprint layout for adding contact and create model

---
+++ app/src/main/java/com/example/contracttracing/ContactModel.kt
@@ -0,0 +1,11 @@
+package com.example.contracttracing
+
+class ContactModel {
+    private val name: String
+    private val number: String
+
+    constructor(name: String, number: String){
+        this.name = name
+        this.number = number
+    }
+}
